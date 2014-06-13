1f73677015305cfd8c1efa9796338a7ccc1e3006
Sean OMeara <someara@opscode.com>
2014-06-13 19:18:20 -0400

    adding mpm_info

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -241,6 +241,97 @@ module Opscode
           }
         end
       end
+
+      # http://httpd.apache.org/docs/2.2/mod/mpm_common.html
+      # http://httpd.apache.org/docs/2.4/mod/mpm_common.html
+      class MPMinfo
+        def mpm_info
+          @mpm_info ||= {
+            '2.2' => {
+              :prefork => {
+                :startservers => '5',
+                :minspareservers => '5',
+                :maxspareservers => '10',
+                :maxclients => '150',
+                :maxrequestsperchild => '0',
+                :minsparethreads => nil,
+                :maxsparethreads => nil,
+                :threadlimit => nil,
+                :threadsperchild => nil,
+                :maxrequestworkers => nil,
+                :maxconnectionsperchild => nil,
+              },
+              :worker => {
+                :startservers => '2',
+                :minspareservers => nil,
+                :maxspareservers => nil,
+                :maxclients => '150',
+                :maxrequestsperchild => '0',
+                :minsparethreads => '25',
+                :maxsparethreads => '75',
+                :threadlimit => '64',
+                :threadsperchild => '25',
+                :maxrequestworkers => nil,
+                :maxconnectionsperchild => nil,
+              },
+              :event => {
+                :startservers => '2',
+                :minspareservers => nil,
+                :maxspareservers => nil,
+                :maxclients => '150',
+                :maxrequestsperchild => '0',
+                :minsparethreads => '25',
+                :maxsparethreads => '75',
+                :threadlimit => '64',
+                :threadsperchild => '25',
+                :maxrequestworkers => nil,
+                :maxconnectionsperchild => nil,
+              }
+            },
+            '2.4' => {
+              :prefork => {
+                :startservers => '5',
+                :minspareservers => '5',
+                :maxspareservers => '10',
+                :maxclients => nil,
+                :maxrequestsperchild => nil,
+                :minsparethreads => nil,
+                :maxsparethreads => nil,
+                :threadlimit => nil,
+                :threadsperchild => nil,
+                :maxrequestworkers => '150',
+                :maxconnectionsperchild => '0',
+              },
+              :worker => {
+                :startservers => '2',
+                :minspareservers => nil,
+                :maxspareservers => nil,
+                :maxclients => nil,
+                :maxrequestsperchild => nil,
+                :minsparethreads => '25',
+                :maxsparethreads => '75',
+                :threadlimit => '64',
+                :threadsperchild => '25',
+                :maxrequestworkers => '150',
+                :maxconnectionsperchild => '0',
+              },
+              :event => {
+                :startservers => '2',
+                :minspareservers => nil,
+                :maxspareservers => nil,
+                :maxclients => nil,
+                :maxrequestsperchild => nil,
+                :minsparethreads => '25',
+                :maxsparethreads => '75',
+                :threadlimit => '64',
+                :threadsperchild => '25',
+                :maxrequestworkers => '150',
+                :maxconnectionsperchild => '0',
+              }
+            }
+          }
+        end
+      end
     end
   end
 end
