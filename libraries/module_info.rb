3b5c1365b6ff492d3f2cc8d0c51ac4557022e769
Franklin Webber <franklin.webber@gmail.com>
2014-07-11 14:59:32 -0700

    DSL like structure for defining the package home for modules

--- libraries/module_info.rb
+++ libraries/module_info.rb
@@ -3,8 +3,58 @@ module Opscode
     module Module
       module Helpers
         class ModuleInfo
-          def self.debian_2_2_core
-            %w(
+
+          #
+          # Given a key, which is a hash of criteria (i.e. module name, platform,
+          # version, httpd_version), this method will return for you the package
+          # where that module exists. `Nil` is returned if the key does not match
+          # any of the defined criteria.
+          #
+          # @example Searching for the 'alias' module on debian 10.04 with
+          #   httpd version 2.2
+          #
+          #     ModuleInfo.find(:module => 'alias', :platform_family => 'debian', :version => '10.04', :httpd_version: '2.2')
+          #
+          def self.find(key)
+            found_key = modules_list.keys.find { |lock| key.merge(lock) == key }
+            modules_list[found_key]
+          end
+
+          #
+          # Define what package stores a list of modules based on the any of the
+          # criteria: module platform_family, platform, version, httpd_version.
+          #
+          # @example Module 'ssl' on an Amazon 2014.03 instance using httpd version 2.4 can be found in the package 'mod_ssl'
+          #
+          #    modules for: { platform: "amazon", version: "2014.03", httpd_version: "2.4" },
+          #      are: [ "ssl" ], found_in_package: -> (name) { "mod_#{name}" }
+          #
+          # When defining the criteria you can specify as little or as much
+          # criteria you need. Not specifying the field means that field allows
+          # any value.
+          #
+          # @example Module 'alias' on an Debian instance using httpd version 2.2 can be found in the package 'apache2'
+          #
+          #    modules for: { platform_family: "debian", httpd_version: "2.2" },
+          #      are: [ "alias" ], found_in_package: -> (name) { "apache2" }
+          #
+          #
+          # This states that the 'alias' module is found on any version
+          # (e.g. 7, 10.04, 12.04) of Debian with httpd version 2.2.
+          #
+          def self.modules(options)
+            options[:are].each do |mod|
+              key = options[:for].merge(module: mod)
+              modules_list[key] = options[:found_in_package].call(mod)
+            end
+          end
+
+          def self.modules_list
+            @modules_list ||= {}
+          end
+
+          modules for: { platform_family: "debian", httpd_version: "2.2" },
+            are: %w(
               actions alias asis auth_basic auth_digest authn_alias authn_anon
               authn_dbd authn_dbm authn_default authn_file authnz_ldap authz_dbm
               authz_default authz_groupfile authz_host authz_owner authz_user
@@ -15,11 +65,11 @@ module Opscode
               proxy_ftp proxy_http proxy_scgi proxy reqtimeout rewrite setenvif
               speling ssl status substitute suexec unique_id userdir usertrack
               vhost_alias
-            )
-          end
+            ),
+          found_in_package: -> (name) { "apache2" }
 
-          def self.debian_2_2_other
-            %w(
+          modules for: { platform_family: "debian", httpd_version: "2.2" },
+            are: %w(
               apparmor apreq2 auth_cas auth_kerb auth_memcookie auth_mysql
               auth_ntlm_winbind auth_openid auth_pam auth_pgsql auth_plain
               auth_pubtkt auth_radius auth_sys_group auth_tkt authn_sasl authn_webid
@@ -31,52 +81,52 @@ module Opscode
               python_doc qos random removeip rivet rivet_doc rpaf ruby ruid2 ruwsgi
               ruwsgi_dbg scgi shib2 spamhaus speedycgi suphp upload_progress uwsgi
               uwsgi_dbg vhost_hash_alias vhost_ldap wsgi wsgi_py3 xsendfile
-            )
-          end
+            ),
+            found_in_package: -> (name) { "libapache2-mod-#{name.gsub('_','-')}" }
 
-          def self.debian_2_4_core
-            %w(
-              access_compat actions alias allowmethods asis auth_basic
-              auth_digest auth_form authn_anon authn_core authn_dbd authn_dbm
-              authn_file authn_socache authnz_ldap authz_core authz_dbd authz_dbm
-              authz_groupfile authz_host authz_owner authz_user autoindex buffer
-              cache cache_disk cache_socache cgi cgid charset_lite data dav
-              dav_fs dav_lock dbd deflate dialup dir dumpio echo env expires
-              ext_filter file_cache filter headers heartbeat heartmonitor include
-              info lbmethod_bybusyness lbmethod_byrequests lbmethod_bytraffic
-              lbmethod_heartbeat ldap log_debug log_forensic macro mime mime_magic
-              mpm_event mpm_prefork mpm_worker negotiation proxy proxy_ajp
-              proxy_balancer proxy_connect proxy_express proxy_fcgi proxy_fdpass
-              proxy_ftp proxy_html proxy_http proxy_scgi proxy_wstunnel ratelimit
-              reflector remoteip reqtimeout request rewrite sed session
-              session_cookie session_crypto session_dbd setenvif slotmem_plain
-              slotmem_shm socache_dbm socache_memcache socache_shmcb speling ssl
-              status substitute suexec unique_id userdir usertrack vhost_alias
-              xml2enc
-            )
-          end
+          modules for: { platform_family: "debian", httpd_version: "2.4" },
+            are: %w(
+                access_compat actions alias allowmethods asis auth_basic
+                auth_digest auth_form authn_anon authn_core authn_dbd authn_dbm
+                authn_file authn_socache authnz_ldap authz_core authz_dbd authz_dbm
+                authz_groupfile authz_host authz_owner authz_user autoindex buffer
+                cache cache_disk cache_socache cgi cgid charset_lite data dav
+                dav_fs dav_lock dbd deflate dialup dir dumpio echo env expires
+                ext_filter file_cache filter headers heartbeat heartmonitor include
+                info lbmethod_bybusyness lbmethod_byrequests lbmethod_bytraffic
+                lbmethod_heartbeat ldap log_debug log_forensic macro mime mime_magic
+                mpm_event mpm_prefork mpm_worker negotiation proxy proxy_ajp
+                proxy_balancer proxy_connect proxy_express proxy_fcgi proxy_fdpass
+                proxy_ftp proxy_html proxy_http proxy_scgi proxy_wstunnel ratelimit
+                reflector remoteip reqtimeout request rewrite sed session
+                session_cookie session_crypto session_dbd setenvif slotmem_plain
+                slotmem_shm socache_dbm socache_memcache socache_shmcb speling ssl
+                status substitute suexec unique_id userdir usertrack vhost_alias
+                xml2enc
+              ),
+            found_in_package: -> (name) { "apache2" }
 
-          def self.debian_2_4_other
-            %w(
-              apparmor auth_mysql auth_pgsql auth_plain perl2 perl2_dev
-              perl2_doc php5 python python_doc wsgi reload_perl fastcgi
-              authcassimple_perl authcookie_perl authenntlm_perl apreq2 auth_cas
-              auth_kerb auth_mellon auth_memcookie auth_ntlm_winbind auth_openid
-              auth_pubtkt auth_radius auth_tkt authn_sasl authn_webid
-              authn_yubikey authnz_external authz_unixgroup axis2c bw dacs
-              defensible dnssd encoding evasive fcgid fcgid_dbg geoip gnutls jk
-              ldap_userdir ldap_userdir_dbg lisp log_slow log_sql log_sql_dbi
-              log_sql_mysql log_sql_ssl mapcache mime_xattr mono musicindex neko
-              netcgi_apache nss parser3 passenger php5filter qos removeip rivet
-              rivet_doc rpaf ruid2 ruwsgi ruwsgi_dbg scgi security2 shib2
-              spamhaus suphp svn upload_progress uwsgi uwsgi_dbg vhost_ldap
-              watchcat webauth webauthldap webkdc wsgi_py3 xsendfile modsecurity
-              mpm_itk request_perl sitecontrol_perl svn webauth webkdc
-            )
-          end
+          modules for: { platform_family: "debian", httpd_version: "2.4" },
+            are: %w(
+                apparmor auth_mysql auth_pgsql auth_plain perl2 perl2_dev
+                perl2_doc php5 python python_doc wsgi reload_perl fastcgi
+                authcassimple_perl authcookie_perl authenntlm_perl apreq2 auth_cas
+                auth_kerb auth_mellon auth_memcookie auth_ntlm_winbind auth_openid
+                auth_pubtkt auth_radius auth_tkt authn_sasl authn_webid
+                authn_yubikey authnz_external authz_unixgroup axis2c bw dacs
+                defensible dnssd encoding evasive fcgid fcgid_dbg geoip gnutls jk
+                ldap_userdir ldap_userdir_dbg lisp log_slow log_sql log_sql_dbi
+                log_sql_mysql log_sql_ssl mapcache mime_xattr mono musicindex neko
+                netcgi_apache nss parser3 passenger php5filter qos removeip rivet
+                rivet_doc rpaf ruid2 ruwsgi ruwsgi_dbg scgi security2 shib2
+                spamhaus suphp svn upload_progress uwsgi uwsgi_dbg vhost_ldap
+                watchcat webauth webauthldap webkdc wsgi_py3 xsendfile modsecurity
+                mpm_itk request_perl sitecontrol_perl svn webauth webkdc
+              ),
+            found_in_package: -> (name) { "libapache2-mod-#{name.gsub('_','-')}" }
 
-          def self.rhel_5_2_2_core
-            %w(
+          modules for: { platform_family: "rhel", version: "5", httpd_version: "2.2" },
+            are: %w(
               actions alias asis auth_basic auth_digest authn_alias authn_anon
               authn_dbd authn_dbm authn_default authn_file authnz_ldap
               authz_dbm authz_default authz_groupfile authz_host authz_owner
@@ -87,18 +137,18 @@ module Opscode
               proxy_balancer proxy_connect proxy_ftp proxy_http reqtimeout rewrite
               setenvif speling status substitute suexec unique_id userdir
               usertrack version vhost_alias
-            )
-          end
+            ),
+            found_in_package: -> (name) { "httpd" }
 
-          def self.rhel_5_2_2_other
-            %w(
+          modules for: { platform_family: "rhel", version: "5", httpd_version: "2.2" },
+            are: %w(
               auth_mysql ssl auth_kerb auth_pgsql authz_ldap dav_svn mono nss
               perl perl-devel perl-devel python revocator
-            )
-          end
+            ),
+            found_in_package: -> (name) { "mod_#{name}"}
 
-          def self.rhel_6_2_2_core
-            %w(
+          modules for: { platform_family: "rhel", version: "6", httpd_version: "2.2" },
+            are: %w(
               actions alias asis auth_basic auth_digest authn_alias authn_anon
               authn_dbd authn_dbm authn_default authn_file authnz_ldap authz_dbm
               authz_default authz_groupfile authz_host authz_owner authz_user
@@ -109,18 +159,18 @@ module Opscode
               proxy_ftp proxy_http proxy_scgi reqtimeout rewrite setenvif speling
               status substitute suexec unique_id userdir usertrack version
               vhost_alias
-            )
-          end
+            ),
+            found_in_package: -> (name) { "httpd" }
 
-          def self.rhel_6_2_2_other
-            %w(
+          modules for: { platform_family: "rhel", version: "6", httpd_version: "2.2" },
+            are: %w(
               perl-devel perl-devel auth_kerb auth_mysql auth_pgsql authz_ldap
               dav_svn dnssd nss perl revocator revocator ssl wsgi
-            )
-          end
+            ),
+            found_in_package: -> (name) { "mod_#{name}"}
 
-          def self.amazon_2_2_core
-            %w(
+          modules for: { platform_family: "rhel", version: "2014.03", httpd_version: "2.2" },
+            are: %w(
               actions alias asis auth_basic auth_digest authn_alias authn_anon
               authn_dbd authn_dbm authn_default authn_file authnz_ldap authz_dbm
               authz_default authz_groupfile authz_host authz_owner authz_user
@@ -131,19 +181,20 @@ module Opscode
               proxy_connect proxy_ftp proxy_http proxy_scgi reqtimeout rewrite
               setenvif speling status substitute suexec unique_id userdir
               usertrack version vhost_alias
-            )
-          end
+            ),
+            found_in_package: -> (name) { "httpd" }
 
-          def self.amazon_2_2_other
-            %w(
+          modules for: { platform_family: "rhel", version: "2014.03", httpd_version: "2.2" },
+            are: %w(
               perl-devel security_crs-extras auth_kerb auth_mysql auth_pgsql
               authz_ldap dav_svn fcgid geoip nss perl proxy_html python security
               security_crs ssl wsgi
-            )
-          end
+            ),
+            found_in_package: -> (name) { "mod_#{name}" }
 
-          def self.amazon_2_4_core
-            %w(
+
+          modules for: { platform_family: "rhel", version: "2014.03", httpd_version: "2.4" },
+            are: %w(
               access_compat actions alias allowmethods asis auth_basic
               auth_digest authn_anon authn_core authn_dbd authn_dbm authn_file
               authn_socache authz_core authz_dbd authz_dbm authz_groupfile
@@ -160,18 +211,18 @@ module Opscode
               rewrite sed setenvif slotmem_plain slotmem_shm socache_dbm
               socache_memcache socache_shmcb speling status substitute suexec
               unique_id unixd userdir usertrack version vhost_alias watchdog
-            )
-          end
+            ),
+            found_in_package: -> (name) { "httpd" }
 
-          def self.amazon_2_4_other
-            %w(
+          modules for: { platform_family: "rhel", version: "2014.03", httpd_version: "2.4" },
+            are: %w(
               auth_kerb fcgid geoip ldap nss perl proxy_html security session
               ssl wsgi wsgi_py27
-            )
-          end
+            ),
+            found_in_package: -> (name) { "mod_#{name}" }
 
-          def self.fedora_20_2_4_core
-            %w(
+          modules for: { platform_family: "fedora", version: "20", httpd_version: "2.4" },
+            are: %w(
               access_compat actions alias allowmethods asis auth_basic
               auth_digest authn_anon authn_core authn_dbd authn_dbm authn_file
               authn_socache authz_core authz_dbd authz_dbm authz_groupfile
@@ -188,11 +239,11 @@ module Opscode
               rewrite sed setenvif slotmem_plain slotmem_shm socache_dbm
               socache_memcache socache_shmcb speling status substitute suexec
               systemd unique_id unixd userdir usertrack version vhost_alias watchdog
-            )
-          end
+            ),
+            found_in_package: -> (name) { "httpd" }
 
-          def self.fedora_20_2_4_other
-            %w(
+          modules for: { platform_family: "fedora", version: "20", httpd_version: "2.4" },
+            are: %w(
               annodex auth_cas auth_kerb auth_mellon auth_ntlm_winbind
               authnz_external authnz_pam auth_token auth_xradius autoindex_mb
               bw cluster cluster-java cluster-javadoc dav_svn dnssd evasive
@@ -202,33 +253,9 @@ module Opscode
               proxy_html proxy_uwsgi qos revocator revocator security
               security_crs security_crs-extras selinux session speedycgi ssl
               suphp wsgi wso2-axis2 xsendfile
-            )
-          end
-        end
+            ),
+            found_in_package: -> (name) { "mod_#{name}" }
 
-        class MethodInfo
-          def self.method_info
-            @method_info ||= {
-              'debian' => {
-                '7' => { '2.2' => 'debian_2_2' },
-                'jessie' => { '2.4' => 'debian_2_4' },
-                '10.04' => { '2.2' => 'debian_2_2' },
-                '12.04' => { '2.2' => 'debian_2_2' },
-                '14.04' => { '2.4' => 'debian_2_4' }
-              },
-              'rhel' => {
-                '5' => { '2.2' => 'rhel_5_2_2' },
-                '6' => { '2.2' => 'rhel_6_2_2' },
-                '2014.03' => {
-                  '2.2' => 'amazon_2_2',
-                  '2.4' => 'amazon_2_4'
-                }
-              },
-              'fedora' => {
-                '20' => { '2.4' => 'fedora_20_2_4' }
-              }
-            }
-          end
         end
 
         def keyname_for(platform, platform_family, platform_version)
@@ -248,35 +275,13 @@ module Opscode
         end
 
         def package_name_for_module(name, httpd_version, platform, platform_family, platform_version)
-          # if platform == 'fedora'
-          #   require 'pry' ; binding.pry
-          # end
+          ModuleInfo.find(platform_family: platform_family,
+            version: keyname_for(platform, platform_family, platform_version),
+            httpd_version: httpd_version,
+            module: name)
 
-          keyname = keyname_for(platform, platform_family, platform_version)
-          method_name = MethodInfo.method_info[platform_family][keyname][httpd_version]
-
-          unless  method_name.nil?
-            case platform_family
-            when 'debian'
-              if ModuleInfo.send("#{method_name}_core").include? name
-                'apache2'
-              elsif ModuleInfo.send("#{method_name}_other").include? name
-                "libapache2-mod-#{name.gsub('_', '-')}"
-              else
-                nil
-              end
-            when /^(rhel|fedora)$/
-              if ModuleInfo.send("#{method_name}_core").include? name
-                'httpd'
-              elsif ModuleInfo.send("#{method_name}_other").include? name
-                "mod_#{name}"
-              else
-                nil
-              end
-            end
-
-          end
         end
+
       end
     end
   end
