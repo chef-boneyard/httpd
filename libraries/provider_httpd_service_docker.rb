a8e40c34365c1af6217cc19e87d34b2946979e86
Sean OMeara <someara@opscode.com>
2014-10-07 17:21:36 -0400

    removing metal. adding docker cookbook

--- libraries/provider_httpd_service_docker.rb
+++ libraries/provider_httpd_service_docker.rb
@@ -5,28 +5,7 @@ class Chef
     class HttpdService
       class Docker < Chef::Provider::HttpdService
         action :create do
-          recipe_eval do
-            run_context.include_recipe 'build-essential'
-          end
-
-          chef_gem 'chef-metal'
-          chef_gem 'chef-metal-docker'
-          
-          require 'chef_metal'
-          require 'chef_metal_docker'
-
-          machine 'wat' do
-            driver 'docker'
-            action :setup
-            machine_options :docker_options => {
-              :base_image => {
-                :name => 'busybox',
-                :repository => 'busybox',
-              },
-              :command => 'nc -l -p 8080',
-              :ports => [ '80:8080']
-            }
-          end
+          docker_image 'busybox'
         end
 
         action :delete do
