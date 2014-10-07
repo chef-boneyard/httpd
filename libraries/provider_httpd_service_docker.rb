1437dd9afd7664f71baf564e976e7929890b96aa
Sean OMeara <someara@opscode.com>
2014-10-06 21:17:26 -0400

    trying some things. no dice

--- libraries/provider_httpd_service_docker.rb
+++ libraries/provider_httpd_service_docker.rb
@@ -4,27 +4,29 @@ class Chef
   class Provider
     class HttpdService
       class Docker < Chef::Provider::HttpdService
-        action :create do         
+        action :create do
           recipe_eval do
             run_context.include_recipe 'build-essential'
           end
-          
-          recipe_eval do
-            run_context.include_recipe 'chef-metal'
-          end
 
+          chef_gem 'chef-metal'
           chef_gem 'chef-metal-docker'
           
+          require 'chef_metal'
+          require 'chef_metal_docker'
+
           machine 'wat' do
             driver 'docker'
-            machine_options :docker_options => {              
+            action :setup
+            machine_options :docker_options => {
               :base_image => {
-                :name => 'busybox'
+                :name => 'busybox',
+                :repository => 'busybox',
               },
-              :command => 'nc -l -p 80'
+              :command => 'nc -l -p 8080',
+              :ports => [ '80:8080']
             }
           end
-          
         end
 
         action :delete do
@@ -32,10 +34,10 @@ class Chef
 
         action :restart do
         end
-        
+
         action :reload do
         end
-        
+
       end
     end
   end
