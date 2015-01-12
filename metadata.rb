3daad14dd51b336d66a7969fca4d455b7f7b0416
Sean OMeara <sean@chef.io>
2015-01-12 09:35:08 -0500

    adding platform support to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,13 @@
 name 'httpd'
 version '0.2.0'
+
+supports 'amazon'
+supports 'redhat'
+supports 'centos'
+supports 'scientific'
+supports 'fedora'
+supports 'debian'
+supports 'ubuntu'
+# supports 'smartos'
+# supports 'omnios'
+# supports 'suse'
