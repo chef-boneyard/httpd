d3659c19957ed3eb023be6d7414d1b220ca82455
Sean OMeara <sean@chef.io>
2015-01-12 09:44:43 -0500

    adding license and description metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,9 @@
 name 'httpd'
 version '0.2.1'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@getchef.com'
+license 'Apache 2.0'
+description 'Provides httpd_service, httpd_config, and mysql_module resources'
 
 supports 'amazon'
 supports 'redhat'
