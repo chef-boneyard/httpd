a00c7a1a7cc92d3b71e080c9c473606892da921c
Sean OMeara <sean@chef.io>
2015-04-06 16:32:49 -0400

    adding a few more examples to httpd_module_test

--- test/fixtures/cookbooks/httpd_module_test/recipes/default.rb
+++ test/fixtures/cookbooks/httpd_module_test/recipes/default.rb
@@ -5,6 +5,16 @@ httpd_module 'auth_basic' do
   action :create
 end
 
+httpd_module 'proxy' do
+  httpd_version node['httpd']['version']
+  action :create
+end
+
+httpd_module 'proxy_fcgi' do
+  httpd_version node['httpd']['version']
+  action :create
+end
+
 httpd_module 'auth_kerb' do
   httpd_version node['httpd']['version']
   action :create
