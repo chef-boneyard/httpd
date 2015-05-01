645c27308c49b6af120094597ec7bbebc0ae6ce2
Sean OMeara <sean@chef.io>
2015-05-01 16:03:11 -0400

    removing proxy_fcgi from test, since its 2.4. only

--- test/fixtures/cookbooks/httpd_module_test/recipes/default.rb
+++ test/fixtures/cookbooks/httpd_module_test/recipes/default.rb
@@ -10,10 +10,10 @@ httpd_module 'proxy' do
   action :create
 end
 
-httpd_module 'proxy_fcgi' do
-  httpd_version node['httpd']['version']
-  action :create
-end
+# httpd_module 'proxy_fcgi' do
+#   httpd_version node['httpd']['version']
+#   action :create
+# end
 
 httpd_module 'auth_kerb' do
   httpd_version node['httpd']['version']
