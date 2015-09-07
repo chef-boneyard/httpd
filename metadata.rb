86405e1433fc12e170709ef77f83abd6e2ac2d58
Tim Smith <tsmith84@gmail.com>
2015-09-07 14:40:45 -0700

    Fix Chef 11 compat

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Provides httpd_service, httpd_config, and httpd_module resources'
-source_url 'https://github.com/chef-cookbooks/httpd'
-issues_url 'https://github.com/chef-cookbooks/httpd/issues'
+source_url 'https://github.com/chef-cookbooks/httpd' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/httpd/issues' if respond_to?(:issues_url)
 
 supports 'amazon'
 supports 'redhat'
