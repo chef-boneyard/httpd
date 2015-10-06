fac2dc4fcfac3e55ea015be6381eec8064f73ba4
Sean OMeara <sean@chef.io>
2015-10-06 15:17:02 -0400

    removing kind_of

--- libraries/httpd_service.rb
+++ libraries/httpd_service.rb
@@ -1,33 +1,33 @@
 module HttpdCookbook
   class HttpdService < ChefCompat::Resource
-    property :contact, kind_of: String, default: 'webmaster@localhost'
-    property :hostname_lookups, kind_of: String, default: 'off'
-    property :instance, kind_of: String, name_property: true
-    property :keepalive, kind_of: [TrueClass, FalseClass], default: true
-    property :keepalivetimeout, kind_of: [String, NilClass], default: '5'
-    property :listen_addresses, kind_of: [String, Array, NilClass], default: ['0.0.0.0']
-    property :listen_ports, kind_of: [String, Array, NilClass], default: %w(80)
-    property :log_level, kind_of: [String, NilClass], default: 'warn'
-    property :maxclients, kind_of: [String, NilClass], default: nil
-    property :maxconnectionsperchild, kind_of: [String, NilClass], default: nil
-    property :maxkeepaliverequests, kind_of: [String, NilClass], default: '100'
-    property :maxrequestsperchild, kind_of: [String, NilClass], default: nil
-    property :maxrequestworkers, kind_of: [String, NilClass], default: nil
-    property :maxspareservers, kind_of: [String, NilClass], default: nil
-    property :maxsparethreads, kind_of: [String, NilClass], default: nil
-    property :minspareservers, kind_of: [String, NilClass], default: nil
-    property :minsparethreads, kind_of: [String, NilClass], default: nil
-    property :modules, kind_of: [Array, NilClass], default: nil
-    property :mpm, kind_of: [String, NilClass], default: nil
-    property :package_name, kind_of: [String, NilClass], default: nil
-    property :run_group, kind_of: String, default: nil
-    property :run_user, kind_of: [String, NilClass], default: nil
-    property :servername, kind_of: [String, NilClass], default: nil
-    property :startservers, kind_of: [String, NilClass], default: nil
-    property :threadlimit, kind_of: [String, NilClass], default: nil
-    property :threadsperchild, kind_of: [String, NilClass], default: nil
-    property :timeout, kind_of: [String, NilClass], default: '400'
-    property :version, kind_of: [String, NilClass], default: nil
+    property :contact, String, default: 'webmaster@localhost'
+    property :hostname_lookups, String, default: 'off'
+    property :instance, String, name_property: true
+    property :keepalive, [TrueClass, FalseClass], default: true
+    property :keepalivetimeout, [String, nil], default: '5'
+    property :listen_addresses, [String, Array, nil], default: ['0.0.0.0']
+    property :listen_ports, [String, Array, nil], default: %w(80)
+    property :log_level, [String, nil], default: 'warn'
+    property :maxclients, [String, nil], default: nil
+    property :maxconnectionsperchild, [String, nil], default: nil
+    property :maxkeepaliverequests, [String, nil], default: '100'
+    property :maxrequestsperchild, [String, nil], default: nil
+    property :maxrequestworkers, [String, nil], default: nil
+    property :maxspareservers, [String, nil], default: nil
+    property :maxsparethreads, [String, nil], default: nil
+    property :minspareservers, [String, nil], default: nil
+    property :minsparethreads, [String, nil], default: nil
+    property :modules, [Array, nil], default: nil
+    property :mpm, [String, nil], default: nil
+    property :package_name, [String, nil], default: nil
+    property :run_group, String, default: nil
+    property :run_user, [String, nil], default: nil
+    property :servername, [String, nil], default: nil
+    property :startservers, [String, nil], default: nil
+    property :threadlimit, [String, nil], default: nil
+    property :threadsperchild, [String, nil], default: nil
+    property :timeout, [String, nil], default: '400'
+    property :version, [String, nil], default: nil
 
     declare_action_class.class_eval { include HttpdCookbook::Helpers }
   end
