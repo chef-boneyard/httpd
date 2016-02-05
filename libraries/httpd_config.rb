6965da42da21389ea70ee608d1b6b9780eef1ea5
Craig Menning <cmenning@novatexsolutions.com>
2016-02-05 09:07:57 -0600

    Fix invalid default value warnings

--- libraries/httpd_config.rb
+++ libraries/httpd_config.rb
@@ -1,11 +1,11 @@
 module HttpdCookbook
   class HttpdConfig < ChefCompat::Resource
     property :config_name, String, name_property: true, required: true
-    property :cookbook, String, default: nil
+    property :cookbook, [String, nil], default: nil
     property :httpd_version, String, default: lazy { default_apache_version }
     property :instance, String, default: 'default'
-    property :source, String, default: nil
-    property :variables, [Hash], default: nil
+    property :source, [String, nil], default: nil
+    property :variables, [Hash], default: {}
 
     include HttpdCookbook::Helpers
   end
