55d694b721f51bb9852c0af0b2b4b4387b927a67
Tim Smith <tsmith84@gmail.com>
2016-05-01 16:10:05 -0700

    Remove extra test files

--- test/cookbooks/httpd_module_test/recipes/default.rb
+++ test/cookbooks/httpd_module_test/recipes/default.rb
@@ -1,5 +1,3 @@
-# comments!
-
 httpd_module 'auth_basic' do
   httpd_version node['httpd']['version']
   action :create
@@ -9,11 +7,6 @@ httpd_module 'proxy' do
   httpd_version node['httpd']['version']
   action :create
 end
-
-# httpd_module 'proxy_fcgi' do
-#   httpd_version node['httpd']['version']
-#   action :create
-# end
 
 httpd_module 'auth_kerb' do
   httpd_version node['httpd']['version']
