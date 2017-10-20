63d1c44d3e52095501d7692f5182deea651beaad
Tim Smith <tsmith@chef.io>
2017-10-20 11:21:11 -0700

    Remove the platform priority map logic
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/provider_priority_rhel.rb
+++
@@ -1,9 +0,0 @@
-require 'chef/platform/provider_priority_map'
-
-if defined? Chef::Platform::ResourcePriorityMap
-  Chef::Platform::ResourcePriorityMap.instance.priority(
-    :httpd_service,
-    [HttpdCookbook::HttpdServiceRhelSystemd, HttpdCookbook::HttpdServiceRhelSysvinit],
-    platform_family: 'rhel'
-  )
-end
